a490cb5dbfe8d3809fbe3b6aae62fefb3235a1d4
razumforce <razum.force@gmail.com>
2017-10-05 21:43:51 +0000

    postgres working

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171005210758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "moderator", default: false, null: false
+    t.boolean "creator", default: false, null: false
+    t.boolean "active", default: true, null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
+  end
 
 end
