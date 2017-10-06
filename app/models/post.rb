95462957dbcfe3f863a3caa4b4e63262710f20ad
razumforce <razum.force@gmail.com>
2017-10-06 19:33:45 +0000

    add validates title uniqueness to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :title, length: {minimum: 5}
+  validates :title, uniqueness: true
   validates :body, presence: true
   validates :user, presence: true
 end
