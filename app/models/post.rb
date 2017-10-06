c7e5fa518346aec14adf0b3c639355da73e45f7d
razumforce <razum.force@gmail.com>
2017-10-06 17:28:29 +0000

    add validation for post and commentary models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :title, length: {minimum: 5}
+  validates :body, presence: true
+  validates :user, presence: true
 end
